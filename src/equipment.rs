470f738b30894f39e1192fac21b15a4af5db8099
Quentin Michel <quentinmichel69110@gmail.com>
2019-10-20 14:07:24 +0200

    Modify way of auto-certified

--- src/equipment.rs
+++ src/equipment.rs
@@ -30,7 +30,7 @@ impl Equipment {
             certificate: None,
             rsa,
         };
-        eq.certificate = Some(Certificate::self_certified(&eq));
+        eq.self_certify();
         let public_key = PKey::from_rsa(rsa_verify).unwrap();
         match eq.certificate.as_ref().unwrap().0.verify(&public_key).unwrap() {
             true => Ok(eq),
@@ -40,15 +40,13 @@ impl Equipment {
     pub fn get_name(&self) -> String {
         self.name.clone()
     }
-    pub fn get_public_key(&self) -> Vec<u8> {
-        self.rsa.public_key_to_pem().unwrap()
-    }
+    pub fn get_public_key(&self) -> Vec<u8> { self.rsa.public_key_to_pem().unwrap() }
     pub fn get_private_key(&self) -> Vec<u8> {
         self.rsa.private_key_to_pem().unwrap()
     }
-    pub fn get_socket_address(&self) -> SocketAddr {
-        SocketAddr::new(IpAddr::V4(self.address), self.port)
-    }
+    pub fn get_socket_address(&self) -> SocketAddr { SocketAddr::new(IpAddr::V4(self.address), self.port) }
+    pub fn self_certify(&mut self) { self.certificate = Some(self.certify(self.name.clone(), self.get_public_key())); }
+    pub fn certify(&self, subject_name: String, subject_pub_key: Vec<u8>) -> Certificate { Certificate::certify(subject_name, subject_pub_key, self.name.clone(), self.get_private_key()) }
 }
 
 impl Display for Equipment {
@@ -71,20 +69,16 @@ impl Display for Equipment {
 pub struct Certificate(pub X509);
 
 impl Certificate {
-    pub fn self_certified(eq: &Equipment) -> Certificate {
-        Certificate::certify(&eq, &eq)
-    }
-
-    pub fn certify(subject: &Equipment, issuer: &Equipment) -> Certificate {
-        let subject_pkey = PKey::from_rsa(subject.rsa.clone()).unwrap();
-        let issuer_pkey = PKey::from_rsa(issuer.rsa.clone()).unwrap();
+    pub fn certify(subject_name: String, subject_pub_key: Vec<u8>, issuer_name: String, issuer_pri_key: Vec<u8>) -> Certificate {
+        let subject_pkey = PKey::public_key_from_pem(subject_pub_key.as_ref()).unwrap();
+        let issuer_pkey = PKey::private_key_from_pem(issuer_pri_key.as_ref()).unwrap();
 
         let mut subject_name_builder = X509Name::builder().unwrap();
-        subject_name_builder.append_entry_by_nid(Nid::COMMONNAME, subject.name.as_str()).unwrap();
+        subject_name_builder.append_entry_by_nid(Nid::COMMONNAME, subject_name.as_str()).unwrap();
         let subject_name = subject_name_builder.build();
 
         let mut issuer_name_builder = X509Name::builder().unwrap();
-        issuer_name_builder.append_entry_by_nid(Nid::COMMONNAME, issuer.name.as_str()).unwrap();
+        issuer_name_builder.append_entry_by_nid(Nid::COMMONNAME, issuer_name.as_str()).unwrap();
         let issuer_name = issuer_name_builder.build();
 
         let mut builder = X509::builder().unwrap();
